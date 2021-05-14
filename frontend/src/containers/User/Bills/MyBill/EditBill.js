689cadbedf7534769758ab8076c24914910acf5e
Aditya Shah <aditya7395@gmail.com>
2021-05-13 17:22:26 -0700

    Fixed display of edit bills modal

--- frontend/src/containers/User/Bills/MyBill/EditBill.js
+++ frontend/src/containers/User/Bills/MyBill/EditBill.js
@@ -4,17 +4,18 @@ import { withStyles } from "@material-ui/core/styles";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { connect } from "react-redux";
-import { requestBillInfo,requestCurrencyInfo } from "../action";
+import { requestBillInfo, requestCurrencyInfo } from "../action";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Axios from "axios";
-import EditIcon from '@material-ui/icons/Edit';
+import EditIcon from "@material-ui/icons/Edit";
 import TextField from "../../../../components/TextField";
 import { useForm } from "../../../../hooks/useForm";
 import Select from "../../../../components/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 const styles = makeStyles((theme) => ({
   paper: {
     maxWidth: 936,
@@ -39,8 +40,16 @@ const styles = makeStyles((theme) => ({
 }));
 
 function EditBillModal(props) {
-  const { classes, onRequestBankInfo, currencies, user, onrequestCurrencyInfo,onrequestBillInfo,bill} = props;
-  
+  const {
+    classes,
+    onRequestBankInfo,
+    currencies,
+    user,
+    onrequestCurrencyInfo,
+    onrequestBillInfo,
+    bill,
+  } = props;
+
   const [open, setOpen] = React.useState(false);
   const styling = styles();
   const formData = {
@@ -57,33 +66,33 @@ function EditBillModal(props) {
     setValues,
     values,
   } = useForm(
-    {  Description:bill.description,target_currency:bill.target_currency,amount:bill.amount,duedate:bill.dueDate},
-    { Description:false, target_currency:false, amount:false, duedate:false }
+    {
+      Description: bill.description,
+      target_currency: bill.target_currency,
+      amount: bill.amount,
+      duedate: bill.dueDate,
+    },
+    {
+      Description: false,
+      target_currency: false,
+      amount: false,
+      duedate: false,
+    }
   );
   const handleClickOpen = () => setOpen(true);
 
   const handleClose = () => setOpen(false);
 
-  // const handleInput = (event) => {
-  //   event.preventDefault();
-  //   console.log("FormData", formData);
-  //   Axios.post(`/bill?&ID=${bill.id}&Description=${formData["Description"]}&target_currency=${formData["target_currency"]}&amount=${formData["amount"]}&duedate=${formData["duedate"]}`,null).then((response) => {
-  //     if (response.status === 200) {
-  //       console.log("Successfully UPDATED Bank Account");
-  //       setOpen(false);
-  //       onrequestBillInfo();
-  //     } else {
-  //       console.log("Failed");
-  //     }
-  //   });
-  // };
   const handleInput = (event) => {
     formSubmit(event, () => {
       console.log("values", values);
-      var date = new Date(values["duedate"]); 
-      let val =(date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear()
-      Axios.post(`/bill?&ID=${bill.id}&Description=${values["Description"]}&target_currency=${values["target_currency"]}&amount=${values["amount"]}&duedate=${val}`,values)
-      .then((response) => {
+      var date = new Date(values["duedate"]);
+      let val =
+        date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+      Axios.post(
+        `/bill?&ID=${bill.id}&Description=${values["Description"]}&target_currency=${values["target_currency"]}&amount=${values["amount"]}&duedate=${val}`,
+        values
+      ).then((response) => {
         if (response.status === 200) {
           console.log("Successfully UPDATED Bank Account");
           setOpen(false);
@@ -91,153 +100,121 @@ function EditBillModal(props) {
         } else {
           console.log("Failed");
         }
-      })
+      });
     });
   };
 
   useEffect(() => {
     onrequestCurrencyInfo();
   }, []);
-  let disabled = false
-  if(bill.status==="Paid"){
-    disabled = true
+  let disabled = false;
+  if (bill.status === "Paid") {
+    disabled = true;
   }
   return (
-<>
-            <Button
-              style={{ margin: "0 auto", display: "flex" }}
-              variant="outlined"
-              color="primary"
-              onClick={handleClickOpen}
-              disabled={disabled}
-            >
-             <EditIcon/>
+    <>
+      <Button
+        style={{ margin: "0 auto", display: "flex" }}
+        variant="outlined"
+        color="primary"
+        onClick={handleClickOpen}
+        disabled={disabled}
+      >
+        <EditIcon />
+      </Button>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <form
+          className={styling.root}
+          noValidate
+          autoComplete="off"
+          onSubmit={handleInput}
+        >
+          <DialogTitle id="form-dialog-title">Pay Bill</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Please Update out the details</DialogContentText>
+            <TextField
+              autoFocus
+              required
+              pattern="^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$"
+              helperText="Please enter a valid Description"
+              margin="dense"
+              id="country"
+              label="Description"
+              name="Description"
+              fullWidth
+              onChange={handleInputChange}
+              value={values["Description"]}
+              error={errors["Description"]}
+            />
+            <Select
+              required
+              margin="dense"
+              id="country"
+              label="Currency"
+              name="target_currency"
+              fullWidth
+              options={currencies}
+              valueKey="id"
+              displayKey="name"
+              helperText="Please select atleast one"
+              onChange={handleInputChange}
+              value={values["target_currency"]}
+              error={errors["target_currency"]}
+            />
+            <TextField
+              required
+              pattern="^[+]?([.]\d+|\d+([.]\d\d?)?)$"
+              helperText="Please enter a valid Amount"
+              margin="dense"
+              name="amount"
+              label="Amount"
+              fullWidth
+              onChange={handleInputChange}
+              value={values["amount"]}
+              error={errors["amount"]}
+            />
+            <InputLabel id="Data">Due Date</InputLabel>
+            <TextField
+              required
+              type="date"
+              margin="dense"
+              name="duedate"
+              fullWidth
+              onChange={handleInputChange}
+              value={values["duedate"]}
+              error={errors["duedate"]}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
             </Button>
-          
-          <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="form-dialog-title"
-          >
-            <form
-              className={styling.root}
-              noValidate
-              autoComplete="off"
-              onSubmit={handleInput}
-            >
-              <DialogTitle id="form-dialog-title">
-                Pay Bill
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  Please Update out the details 
-                </DialogContentText>
-                {/* <TextField
-                  autoFocus
-                  required
-                  margin="dense"
-                  label="PayEmail"
-                  name="toEmail"
-                  fullWidth
-                  value = {values["toEmail"]}
-                  errors={errors["toEmail"]}
-                  onChange={handleInputChange}
-                /> */}
-                <TextField
-                  autoFocus
-                  required
-                  pattern="^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$"
-                  helperText="Please enter a valid Description"
-                  margin="dense"
-                  id="country"
-                  label="Description"
-                  name="Description"
-                  fullWidth
-                  onChange={handleInputChange}
-                  value = {values["Description"]}
-                  error={errors["Description"]}
-                  
-                />
-                {/* <TextField
-                  autoFocus
-                  required
-                  margin="dense"
-                  name="target_currency"
-                  label="target "
-                  fullWidth
-                  onInput={(e) => (formData[e.target.name] = e.target.value)}
-                /> */}
-                  <Select
-                  required
-                  margin="dense"
-                  id="country"
-                  label="Currency"
-                  name="target_currency"
-                  fullWidth
-                  options = {currencies}
-                  valueKey="id"
-                  displayKey="name"
-                  helperText="Please select atleast one"
-                  onChange={handleInputChange}
-                  value = {values["target_currency"]}
-                  error={errors["target_currency"]}
-                  
-                />
-                <TextField
-                  
-                  required
-                  pattern="^[+]?([.]\d+|\d+([.]\d\d?)?)$"
-                  helperText="Please enter a valid Amount"
-                  margin="dense"
-                  name="amount"
-                  label="Amount"
-                  fullWidth
-                  onChange={handleInputChange}
-                  value = {values["amount"]}
-                  error={errors["amount"]}
-                  
-                />
-                <TextField
-                  
-                  required
-                  type="date"
-                  margin="dense"
-                  name="duedate"
-                  
-                  label="Due Date"
-                  fullWidth
-                  onChange={handleInputChange}
-                  value = {values["duedate"]}
-                  error={errors["duedate"]}
-                  
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} color="primary"> 
-                  Cancel
-                </Button>
-                <Button type="submit" color="primary"disabled={!isFormValid}>
-                  Add Bill
-                </Button>
-              </DialogActions>
-            </form>
-          </Dialog>
-</>
+            <Button type="submit" color="primary" disabled={!isFormValid}>
+              Add Bill
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
     user: state.user,
-    currencies :state.currency.currencies
+    currencies: state.currency.currencies,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    
-    onrequestCurrencyInfo: ()  => dispatch(requestCurrencyInfo()),
-    onrequestBillInfo: () => dispatch(requestBillInfo())
+    onrequestCurrencyInfo: () => dispatch(requestCurrencyInfo()),
+    onrequestBillInfo: () => dispatch(requestBillInfo()),
   };
 };
 
@@ -245,17 +222,6 @@ EditBillModal.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-// export default (props) => (
-//   <ComponentWrapper
-//     name="Bank Account Details"
-//     helperText="View and update your bank account information"
-//     Component={connect(
-//       mapStateToProps,
-//       mapDispatchToProps
-//     )(withStyles(styles)(Content))}
-//     {...props}
-//   />
-// );
 export default connect(
   mapStateToProps,
   mapDispatchToProps
